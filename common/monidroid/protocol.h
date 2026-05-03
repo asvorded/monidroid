374d1c236a8e423a24cb1a6a6a18676fe742c62c
Maksim Dadush <pud700max@gmail.com>
2026-05-03 12:50:20 +0300

    New control protocol values

--- common/monidroid/protocol.h
+++ common/monidroid/protocol.h
@@ -32,6 +32,9 @@ namespace Monidroid {
         inline constexpr char CL_WELCOME_WORD[PROTOCOL_WORD_LEN + 1]      = "CWLCM";
         inline constexpr char CL_USB_WELCOME_WORD[PROTOCOL_WORD_LEN + 1]  = "CUSBW";
 
+        inline constexpr unsigned int ADB_IF_CLASS         = 0xFF4201;
+        inline constexpr unsigned int ADB_MTP_IF_CLASS     = 0x060101;
+
         inline constexpr char CL_INPUT_WORD[PROTOCOL_WORD_LEN + 1]        = "CINPT";
 
         enum class InputType : int {
@@ -57,8 +60,16 @@ namespace Monidroid {
         inline constexpr char NO_EXTENSION_WORD[PROTOCOL_WORD_LEN + 1]    = "MDNOX";
     }
 
-    inline constexpr unsigned int ADB_IF_CLASS         = 0xFF4201;
-    inline constexpr unsigned int ADB_MTP_IF_CLASS     = 0x060101;
+    namespace Control {
+        inline constexpr auto PORT               = 14768;
+
+        inline constexpr auto GET_CLIENTS        = "/clients/all";
+        inline constexpr auto CONNECTED_EVENT    = "/clients/new";
+        inline constexpr auto DISCONNECTED_EVENT = "/clients/disconnected";
+        inline constexpr auto GET_CONFIG         = "/config/all";
+        inline constexpr auto SET_STATE          = "/config/serverState";
+        inline constexpr auto SHUTDOWN           = "/config/shutdown";
+    }
 
     namespace Ex {
         // Delay calculation
--- js-client/src/server/websocket.ts
+++ js-client/src/server/websocket.ts
@@ -1,9 +1,11 @@
 export const wsProtocol = {
-  PORT: 14776,
+  PORT: 14768,
   ALL: 'clients/all',
   CLIENT_CONNECTED: 'clients/new',
   CLIENT_DISCONNECTED: 'clients/disconnected',
   SERVER_STATE: 'config/serverState',
+  SERVER_CONFIG: 'config/all',
+  SHUTDOWN: 'confog/shutdown',
 } as const;
 
 type ConnectionType = "wifi" | "usb";
